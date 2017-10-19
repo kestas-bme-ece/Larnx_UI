8814f812c74568cdbf1772235bd0d1069109829f
Kestutis Subacius <kestas@bu.edu>
2017-10-19 11:03:09 -0400

    Ability to load local content (images and videos) is working
    We can now load local images and videos to the application. And they can be displayed in the UI. Next steps would be to hook up a back-end image proccessing script that does a simple transformation of the image, and then resurns the result + statistics.

--- renderer.js
+++ renderer.js
@@ -1,9 +1,26 @@
 // This file is required by the index.html file and will
 // be executed in the renderer process for that window.
 // All of the Node.js APIs are available in this process.
+const remote = require('electron').remote;
 const ipcRenderer = require('electron').ipcRenderer;
+const dialog = remote.dialog; //require('electron').dialog;
+const fs = require("fs");
+
 
 function render_index() {
     ipcRenderer.send('render_index');
 }
+
+function fileExplorer() {
+    dialog.showOpenDialog(function (fileNames) {
+        if (fileNames === undefined) return;
+
+        var fileName = fileNames[0];
+        fs.readFile(fileName, 'utf-8', function (err, data) {
+            document.getElementById("original").src=fileName
+    });
+    });
+}
+
+
 
