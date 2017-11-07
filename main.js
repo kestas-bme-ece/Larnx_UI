2c5fb20534c7b4eda780752bbebc3b6e51a7843b
Kestutis Subacius <kestas@bu.edu>
2017-11-07 13:05:03 -0500

    Updates the UI CSS, Added Video Player, and Integrated Backend Executables

--- main.js
+++ main.js
@@ -27,7 +27,7 @@ function createMainWindow () {
   mainWindow.webContents.openDevTools();
 
   // Emitted when the window is closed.
-  mainWindow.on('closed', function () {
+    mainWindow.on('closed', function () {
     // Dereference the window object, usually you would store windows
     // in an array if your app supports multi windows, this is the time
     // when you should delete the corresponding element.
--- renderer.js
+++ renderer.js
@@ -1,26 +1,64 @@
 // This file is required by the index.html file and will
 // be executed in the renderer process for that window.
 // All of the Node.js APIs are available in this process.
-const remote = require('electron').remote;
-const ipcRenderer = require('electron').ipcRenderer;
-const dialog = remote.dialog; //require('electron').dialog;
-const fs = require("fs");
-
+const remote        = require('electron').remote;
+const ipcRenderer   = require('electron').ipcRenderer;
+const dialog        = remote.dialog; //require('electron').dialog;
+const fs            = require("fs");
+const path          = require('path');
+const videojs       = require('video.js');
 
 function render_index() {
     ipcRenderer.send('render_index');
 }
 
-function fileExplorer() {
+function fileExplorer(import_request) {
     dialog.showOpenDialog(function (fileNames) {
         if (fileNames === undefined) return;
 
         var fileName = fileNames[0];
-        fs.readFile(fileName, 'utf-8', function (err, data) {
-            document.getElementById("original").src=fileName
+        fs.readFile(fileName, 'base64', function (error, data) {
+            if ( path.extname(fileName)  == '.jpg'|| path.extname(fileName)  == '.png' )
+            {
+                document.getElementById("original").src=fileName;
+            }
+
+            if ( path.extname(fileName)  == '.mp4' )
+            {
+                document.getElementById("vid_original_mp4").src="file:///" + fileName;
+                document.getElementById("vid_original_header").setAttribute('data-video-path',fileName);
+                videoPlayer.src({type: "video/mp4", src: "file:///" + fileName});
+            }
     });
     });
 }
 
+function execute() {
+    const PROC = require('child_process').spawn('Executables/Larnx.exe',[ document.getElementById("vid_original_header").getAttribute('data-video-path') ]);
 
+    PROC.stdout.on('data', function(data)
+    {});
 
+    PROC.stderr.on("data", function (data)
+    {});
+
+    PROC.on('close', function (data)
+    {});
+
+    PROC.on('exit', function (data)
+    {});
+}
+
+// Deprecated scripts, notes, trash. Please remove in <MASTER> branch
+
+// FROM RENDER.js
+// document.getElementById("vid_original").type='video/mp4';
+// var videoPlayer = videojs(document.getElementById('vid_original_header'), {}, function(){
+//     this.src({type: "video/mp4", src: "file:///" + fileName});
+// });
+
+// FROM index.js
+//        videojs('vid_original_header').ready(function(){
+//            videoPlayer = this;
+//            videoPlayer.src();
+//        });
